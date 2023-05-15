c175b51e83f38cda2d4d40416b6db4cdf0ec870d
SebastJava <sebastjava@hotmail.ca>
2023-05-15 17:15:59 -0400

    modified:   member-sign.sh member-list.txt (name:function)

--- member-sign.sh
+++ member-sign.sh
@@ -7,11 +7,13 @@ for version in $VERSIONS
 do
 	for member in `cat member-list.txt`
 	do
-		cp $version img_host/$member-$version
-		sed -i "s/MyNameIs15Chars/$member/" img_host/$member-$version
-		echo Rendering img_host/$member-$version
-		$INKSCAPE --export-type="png" img_host/$member-$version # >/dev/null
-		rm img_host/$member-$version
+		name=$(echo $member | cut -d ":" -f 1); title=$(echo $member | cut -d ":" -f 2)
+		cp $version img_host/$name-$version
+		sed -i "s/MyNameIs15Chars/$name/" img_host/$name-$version
+		sed -i "s/Moderator/$title/" img_host/$name-$version
+		echo Rendering img_host/$name-$version
+		$INKSCAPE --export-type="png" img_host/$name-$version # >/dev/null
+		rm img_host/$name-$version
 	done
 done
 
