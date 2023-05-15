e37f57249563d074aa175d28003c04bad3c79b73
SebastJava <sebastjava@hotmail.ca>
2023-05-15 17:32:58 -0400

    modified:   member-sign.sh
    renamed:    member-list.txt -> members-list.txt

--- member-sign.sh
+++ member-sign.sh
@@ -2,10 +2,11 @@
 
 INKSCAPE="/usr/bin/inkscape"
 VERSIONS="flat-move.svg flat.svg gradient-move.svg gradient-v3.svg gray.svg twotones-move.svg"
+MEMBERS=$(cat members-list.txt)
 
 for version in $VERSIONS
 do
-	for member in `cat member-list.txt`
+	for member in $MEMBERS
 	do
 		name=$(echo $member | cut -d ":" -f 1); title=$(echo $member | cut -d ":" -f 2)
 		cp $version img_host/$name-$version
