fee09c28e3645c0e4c55494d6567ef9cb3ec3a25
SebastJava <sebastjava@hotmail.ca>
2023-05-16 17:35:40 -0400

    members-sign.sh run (after modifications)
    	new file:   img_host/AZgl1800-sign-flat-move.png
    	new file:   img_host/AZgl1800-sign-flat.png
    	new file:   img_host/AZgl1800-sign-gradient-move.png
    	new file:   img_host/AZgl1800-sign-gradient-v3.png
    	new file:   img_host/AZgl1800-sign-gray.png
    	new file:   img_host/AZgl1800-sign-twotones-move.png
    	new file:   img_host/sebastjava-sign-flat-move.png
    	new file:   img_host/sebastjava-sign-flat.png
    	new file:   img_host/sebastjava-sign-gradient-move.png
    	new file:   img_host/sebastjava-sign-gradient-v3.png
    	new file:   img_host/sebastjava-sign-gray.png
    	new file:   img_host/sebastjava-sign-twotones-move.png
    	modified:   members-sign.sh

--- members-sign.sh
+++ members-sign.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 INKSCAPE="/usr/bin/inkscape"
-VERSIONS="flat-move.svg flat.svg gradient-move.svg gradient-v3.svg gray.svg twotones-move.svg"
+SOURCES="sign-flat sign-flat-move sign-gradient-move sign-gradient-v3 sign-gray sign-twotones-move"
 MEMBERS=$(cat members-list.txt)
 
-for version in $VERSIONS
+for src in $SOURCES
 do
 	for member in $MEMBERS
 	do
 		name=$(echo $member | cut -d ":" -f 1); title=$(echo $member | cut -d ":" -f 2)
-		cp $version img_host/$name-$version
-		sed -i "s/MyNameIs15Chars/$name/" img_host/$name-$version
-		sed -i "s/MyTitleIs/$title/" img_host/$name-$version
-		echo Rendering img_host/$name-$version
-		$INKSCAPE --export-type="png" img_host/$name-$version # >/dev/null
-		rm img_host/$name-$version
+		cp $src.svg img_host/$name-$src.svg
+		sed -i "s/MyNameIs15Chars/$name/" img_host/$name-$src.svg
+		sed -i "s/MyTitleIs/$title/" img_host/$name-$src.svg
+		echo Rendering img_host/$name-$src
+		$INKSCAPE --export-type="png" img_host/$name-$src.svg # >/dev/null
+		rm img_host/$name-$src.svg
 	done
 done
 
